f8374983a4311a8cef0054e84ceae8b08495965d
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-06-11 23:23:10 +0530

    Add project files.

---
+++ CSharpBasicFuntionality/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharpBasicFuntionality
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isEqual = Test<string>.isTrue("Mahendra","Mahendra");
+            Console.Write(isEqual);
+           
+            ArrayList arr = new ArrayList();
+            arr.Add(1);
+            arr.Add("A");
+            foreach (var a in arr)
+                Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+  class Test<T>
+    {
+      internal  static bool isTrue(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
