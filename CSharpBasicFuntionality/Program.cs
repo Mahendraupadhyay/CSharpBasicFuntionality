1a148c9654a5be74a5b9df8bb0116f8d38b490e3
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-06-12 00:30:50 +0530

    /*Array Sorting  (bubble  sorting)*/

--- CSharpBasicFuntionality/Program.cs
+++ CSharpBasicFuntionality/Program.cs
@@ -7,6 +7,28 @@ namespace CSharpBasicFuntionality
     {
         static void Main()
         {
+            /*Array Sorting  (bubble  sorting)*/
+            int[] arrint = new int[7] { 5,3,6,234,45,33,434};
+            int temp;
+
+            for(int i=0;i<= arrint.Length;i++)
+            {
+                for(int j=0;j< arrint.Length-1;j++)
+                {
+                    if(arrint[j]> arrint[j+1])
+                    {
+                        temp = arrint[j + 1];
+                        arrint[j + 1] = arrint[j];
+                        arrint[j] = temp;
+                    }
+                }
+            }
+            foreach (int u in arrint)
+                Console.WriteLine(u);
+
+
+
+            //******************************************************
             /*Generics example*/
             bool isEqual = Test<string>.isTrue("Mahendra","Mahendra");
             Console.Write(isEqual);
