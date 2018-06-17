f01eb59ca023d041f4ede5df0114dd5d3f548320
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-06-18 01:14:51 +0530

    Usages of Enums

--- CSharpBasicFuntionality/Program.cs
+++ CSharpBasicFuntionality/Program.cs
@@ -10,51 +10,84 @@ namespace CSharpBasicFuntionality
     {
         static void Main()
         {
-            /* Lamda Expression on List of object to fetch a particular item*/
-            List<Customer> list = new List<Customer>();
-            Customer cust = new Customer(1, "A");
-            list.Add(cust);
-            Customer cust1 = new Customer(2, "B");
-            list.Add(cust1);
+            /*Sample Program which uses Enum*/
+            {/* If program uses a set of integer numbers, Consider replacing them with Enum, Which makes program more readable and maintanable*/
+                Customer[] customers = new Customer[3];
+                customers[0] = new Customer { Name = "Mary", GenderType = Gender.Female };
+                customers[1] = new Customer { Name = "Mark", GenderType = Gender.Male };
+                customers[2] = new Customer { Name = "Sam", GenderType = Gender.Unknown };
 
-            var details = list
-                          .Where(p => p.Id ==1).FirstOrDefault();
+                foreach (Customer cust in customers)
+                    Console.WriteLine("Name {0} && Ganeder {1}", cust.Name, GetGender(cust.GenderType));
 
-             Console.WriteLine(details.Id.ToString()+details.Name);
-            //****End********//
-
-            /*Array Sorting  (bubble  sorting)  **Start***/
-            int[] arrint = new int[7] { 5, 3, 6, 234, 45, 33, 434 };
-            int temp;
-
-            for (int i = 0; i <= arrint.Length; i++)
-            {
-                for (int j = 0; j < arrint.Length - 1; j++)
+                string GetGender(Gender gender)
                 {
-                    if (arrint[j] > arrint[j + 1])
+                    switch (gender)
                     {
-                        temp = arrint[j + 1];
-                        arrint[j + 1] = arrint[j];
-                        arrint[j] = temp;
+                        case Gender.Male:
+                            return "Male";
+                        case Gender.Female:
+                            return "Female";
+                        case Gender.Unknown:
+                            return "Unknown";
+                        default:
+                            return "Wrong Data";
                     }
                 }
             }
-            foreach (int u in arrint)
-                Console.WriteLine(u);
-            //****End********//
+            /*****End*****/
+
+            /* Lamda Expression on List of object to fetch a particular item*/
+            {
+                List<Customer> list = new List<Customer>();
+                Customer cust = new Customer(1, "A");
+                list.Add(cust);
+                Customer cust1 = new Customer(2, "B");
+                list.Add(cust1);
+
+                var details = list
+                              .Where(p => p.Id == 1).FirstOrDefault();
+
+                Console.WriteLine(details.Id.ToString() + details.Name);
+            }
+            /****End********/
+
+            /*Array Sorting  (bubble  sorting)  **Start***/
+            {
+                int[] arrint = new int[7] { 5, 3, 6, 234, 45, 33, 434 };
+                int temp;
+
+                for (int i = 0; i <= arrint.Length; i++)
+                {
+                    for (int j = 0; j < arrint.Length - 1; j++)
+                    {
+                        if (arrint[j] > arrint[j + 1])
+                        {
+                            temp = arrint[j + 1];
+                            arrint[j + 1] = arrint[j];
+                            arrint[j] = temp;
+                        }
+                    }
+                }
+                foreach (int u in arrint)
+                    Console.WriteLine(u);
+            }
+            /****End********/
 
             //******************************************************
             /*Generics example*/
-            bool isEqual = Test<string>.isTrue("Mahendra", "Mahendra");
-            Console.Write(isEqual);
+            {
+                bool isEqual = Test<string>.isTrue("Mahendra", "Mahendra");
+                Console.Write(isEqual);
 
-            ArrayList arr = new ArrayList();
-            arr.Add(1);
-            arr.Add("A");
-            foreach (var a in arr)
-                Console.WriteLine(a);
-            Console.ReadKey();
-            //****End********//
+                ArrayList arr = new ArrayList();
+                arr.Add(1);
+                arr.Add("A");
+                foreach (var a in arr)
+                    Console.WriteLine(a);
+                Console.ReadKey();
+            }
+            /****End********/
         }
     }
     class Test<T>
@@ -67,20 +100,24 @@ namespace CSharpBasicFuntionality
 
     class Customer
     {
-        private int id;
-        private string name;
-
         internal Customer(int id1, string name1)
         {
             Id = id1; Name = name1;
         }
-
-        public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
-
+        internal Customer() {}
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Gender GenderType { get; set; }
         public override string ToString()
         {
             return Id + "," + Name;
         }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Unknown
     }
 }
