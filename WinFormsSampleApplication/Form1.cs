29a1ddce887460041c8c2ff08f2f542e2f819935
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-08-12 00:57:17 +0530

    thread implementation

--- WinFormsSampleApplication/Form1.cs
+++ WinFormsSampleApplication/Form1.cs
@@ -19,25 +19,49 @@ namespace WinFormsSampleApplication
             InitializeComponent();
         }
 
+
         private int ReadDataCharacterCount()
         {
             int count = 0;
-                using (StreamReader reader = new StreamReader("C:\\Users\\Mahendra\\Desktop\\Data.txt"))
-                {
-                    string read = reader.ReadToEnd();
-                    count = read.Length;
-                    Thread.Sleep(5000);
-                }
+            using (StreamReader reader = new StreamReader("C:\\Users\\Mahendra\\Desktop\\Data.txt"))
+            {
+                string read = reader.ReadToEnd();
+                count = read.Length;
+                Thread.Sleep(5000);
+            }
             return count;
         }
-        private async void btnProcessing_Click(object sender, EventArgs e)
-        {
-            Task<int> task = new Task<int>(ReadDataCharacterCount);
-            task.Start();
-            lblStatusMessage.Text = "Processing file...";
+        /*Usage of Asyns and Await*/
+        //private async void btnProcessing_Click(object sender, EventArgs e)
+        //{
+        //    Task<int> task = new Task<int>(ReadDataCharacterCount);
+        //    task.Start();
+        //    lblStatusMessage.Text = "Processing file...";
 
-            int count = await task;
-            lblStatusMessage.Text = count.ToString() + " number of character in file";
+        //    int count = await task;
+        //    lblStatusMessage.Text = count.ToString() + " number of character in file";
+        //}
+        /*Usage of Asyns and Await*  **END**/
+        /*Same example using thread*/
+        int countChar = 0;
+        private void btnProcessing_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                countChar = ReadDataCharacterCount();
+                Action action = new Action(SetLabelMessage);
+                this.BeginInvoke(action);
+            }
+            );
+            thread.Start();
+            lblStatusMessage.Text = "Processing file...";
         }
+
+        private void SetLabelMessage()
+        {
+            lblStatusMessage.Text = countChar.ToString() + " number of character in file";
+        }
+        /*END thread examle*/
     }
 }
+
