6252329190248a049f94315d59fcf8724aa22c8d
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-08-12 00:21:33 +0530

    example of Asyn and Await keyword Added WinForm Application.

---
+++ WinFormsSampleApplication/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WinFormsSampleApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcessing = new System.Windows.Forms.Button();
+            this.lblStatusMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcessing
+            // 
+            this.btnProcessing.Location = new System.Drawing.Point(228, 90);
+            this.btnProcessing.Name = "btnProcessing";
+            this.btnProcessing.Size = new System.Drawing.Size(128, 23);
+            this.btnProcessing.TabIndex = 0;
+            this.btnProcessing.Text = "Start Reading File";
+            this.btnProcessing.UseVisualStyleBackColor = true;
+            this.btnProcessing.Click += new System.EventHandler(this.btnProcessing_Click);
+            // 
+            // lblStatusMessage
+            // 
+            this.lblStatusMessage.AutoSize = true;
+            this.lblStatusMessage.Location = new System.Drawing.Point(225, 148);
+            this.lblStatusMessage.Name = "lblStatusMessage";
+            this.lblStatusMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblStatusMessage.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 354);
+            this.Controls.Add(this.lblStatusMessage);
+            this.Controls.Add(this.btnProcessing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcessing;
+        private System.Windows.Forms.Label lblStatusMessage;
+    }
+}
+
---
+++ WinFormsSampleApplication/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WinFormsSampleApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int ReadDataCharacterCount()
+        {
+            int count = 0;
+                using (StreamReader reader = new StreamReader("C:\\Users\\Mahendra\\Desktop\\Data.txt"))
+                {
+                    string read = reader.ReadToEnd();
+                    count = read.Length;
+                    Thread.Sleep(5000);
+                }
+            return count;
+        }
+        private async void btnProcessing_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(ReadDataCharacterCount);
+            task.Start();
+            lblStatusMessage.Text = "Processing file...";
+
+            int count = await task;
+            lblStatusMessage.Text = count.ToString() + " number of character in file";
+        }
+    }
+}
