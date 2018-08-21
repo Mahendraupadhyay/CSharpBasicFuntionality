b95ffd4bcfb3b878c17ccc07c2939532f9416300
Mahendra Upadhyay <mahendraupadhyay14@gmail.com>
2018-08-22 01:11:07 +0530

    Sample WPF MVVM Application

---
+++ SampleWPFApplication/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace SampleWPFApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            View.MainPage window = new View.MainPage();
+            ViewModel.UserViewModel VM = new ViewModel.UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
